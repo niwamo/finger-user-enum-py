86c7b7b0f3fb27ac88ead824d6dde1bc79c250cb
yaboi <yaboi@kalimain>
2022-01-19 00:01:05 -0600

    added example.png

--- finger-user-enum.py
+++ finger-user-enum.py
@@ -9,12 +9,13 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(dest='userfile', help='wordlist to use for potential usernames')
     parser.add_argument(dest='rhost', help='ip address of target host')
+    parser.add_argument('-q', dest='quiet', action='store_true', required=False, help='suppress running output')
     parser.add_argument('-t', dest='threadcount', required=False, default=1, type=int)
     parser.add_argument('-o', dest='out', required=False, help='path to output file')
     return parser.parse_args()
 
 
-def test_users(userlist, found_users, rhost):
+def test_users(userlist, found_users, rhost, quietmode):
     for user in userlist:
         try:
             out = subprocess.check_output(f'finger {user}@{rhost} 2>/dev/null', shell=True).decode('utf-8')
@@ -22,7 +23,8 @@ def test_users(userlist, found_users, rhost):
             for line in outputlines:
                 if len(splitline:=line.strip().split()) > 2 and splitline!=['Login','Name','TTY','Idle','When','Where']:
                     new_user = splitline[0]
-                    print(f"{'-'*80}\nFound: {new_user}\n{line}\n{'-'*80}")
+                    if not quietmode:
+                        print(f"{'-'*80}\nFound: {new_user}\n{line}\n{'-'*80}")
                     found_users.append(new_user)
         except Exception as e:
             print(f"Error while checking {user}:\n\t{e}")
@@ -41,7 +43,7 @@ def main():
         start = i * thread_size
         end = min(start+thread_size, len(users_to_test))
         test_subset = users_to_test[start:end]
-        thrd = threading.Thread(target=test_users, args=(test_subset, found_users, args.rhost,))
+        thrd = threading.Thread(target=test_users, args=(test_subset, found_users, args.rhost, args.quiet,))
         threads.append(thrd)
         thrd.start()
     for thread in threads:
@@ -52,7 +54,7 @@ def main():
             for user in found_users:
                 f.write(user + '\n')
     else:
-        print(f"\n{'-'*40}Script Finished. Found {len(found_users)} usernames.\n{'-'*40}")
+        print(f"\n{'-'*40}\nScript Finished. Found {len(found_users)} usernames.\n{'-'*40}")
         for user in found_users:
             print(user)
 
